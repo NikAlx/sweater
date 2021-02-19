36f7a70b7223e81a59a4877deb286ef46d2f4deb
NikAlx <brr-r@mail.ru>
2021-02-20 02:32:14 +0300

    03 Lesson3
    - add lesson code

---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,41 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+    @Autowired
+    UserRepo userRepo;
+
+    // "/registration" - endpoint
+    @GetMapping
+    public String registration() {
+        // "registration" - view, registration.mustache
+        return "registration";
+    }
+
+    @PostMapping
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.username());
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.active(true).roles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/sweater/domain/User.java
@@ -0,0 +1,28 @@
+package com.example.sweater.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Accessors(fluent = true)
+@Entity
+@Table(name = "usr")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+}
